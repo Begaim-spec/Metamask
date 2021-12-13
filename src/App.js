c8e2e902136616d1512cbe048d6c51bbf0c3504d
Begaim <b.abzhaparkyzy@gmail.com>
2021-12-13 18:39:31 +0600

    Error checking and changeNetwork function was added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {ethers} from "ethers";
 import ers20abi from './erc20.abi.json'
 import './App.css'
@@ -7,64 +7,104 @@ const App = () => {
 
     const [errorMessage, setErrorMessage] = useState(null)
     const [walletText, setWalletText] = useState('Connect wallet')
+    const [balanceError, setBalanceError] = useState('')
+    const [isConnected, setIsConnected] = useState(false)
     const [userInfo, setUserinfo] = useState('')
     const [value, setValue] = useState('')
+    const [chainId, setChainId] = useState(null)
     const [account, setAccount] = useState('')
     const contractAddress = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'
 
+
     const handleConnectWallet = () => {
-        if(window.ethereum){
+        if (window.ethereum) {
             window.ethereum.request({method: 'eth_requestAccounts'})
                 .then((result) => {
                     setWalletText('Wallet connected')
-                    setAccount(result)
-                })
+                    changeAccount(result[0])
+                    setIsConnected(true)
+                });
+            window.ethereum.request({method: 'eth_chainId'})
+                .then((result) => {
+                    changeNetwork(result)
+                });
         } else {
             setErrorMessage('Error. Be sure that MetaMask is installed')
         }
     }
 
-    const handleSubmit =  (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault()
         const data = new FormData(e.target)
         setUserinfo(data.get('address'))
     }
 
-    if (userInfo){
+    const changeAccount = (newAccount) => {
+        setAccount(newAccount)
+    }
+
+    const changeNetwork = (chainId) => {
+        setChainId(chainId)
+    }
+
+    if (userInfo) {
         const getBalance = async () => {
-            const provider = new ethers.providers.Web3Provider(window.ethereum)
-            await provider.send("eth_requestAccounts", [])
-            const erc20 =  new ethers.Contract(contractAddress, ers20abi, provider)
-            try{
+            const provider = await new ethers.providers.Web3Provider(window.ethereum)
+            const erc20 = await new ethers.Contract(contractAddress, ers20abi, provider)
+            try {
                 const balance = await erc20.balanceOf(userInfo)
                 const currentBalance = await parseFloat(ethers.utils.formatEther(balance));
                 setValue(currentBalance)
+                console.log(currentBalance)
             } catch (e) {
-                alert('Invalid address!')
+                setBalanceError('Address not found. Please check and try again')
+            } finally {
+
             }
         }
         getBalance()
     }
 
-    return (
-        <div className='App'>
-            <div className='connect-wallet'>
-                <button className='button' onClick={handleConnectWallet}>{walletText}</button>
-                { account && <h3>Your account address is: {account}</h3>}
-            </div>
-            <form onSubmit={handleSubmit} >
-                <input type="text"
-                       placeholder='Type wallet address'
-                       name='address'
-                       autoComplete='off'
-                       className='input'
-                       required={true}
-                />
-                <button type='submit' className='button'>Get balance </button>
-            </form>
-            {value && <h3>Result: {value}</h3>
+    useEffect(() => {
+        window.ethereum.on('chainChanged', changeNetwork)
+        window.ethereum.on('accountsChanged', changeAccount)
+        return () => {
+            if (isConnected) {
+                window.ethereum.removeListener = () => ('chainChanged', changeNetwork)
+                window.ethereum.removeListener('accountsChanged', changeAccount)
             }
-            <p>{errorMessage}</p>
+        }
+    }, [isConnected])
+
+
+    return (
+        <div>
+            <header className='header'>
+                <h1>Metamask & WEB3</h1>
+                <div className='connect-wallet'>
+                    <button className='button' onClick={handleConnectWallet}>{walletText}</button>
+                </div>
+            </header>
+            <div className='main'>
+                <div className='user-info'>
+                    {account && <h5>Address: {account}</h5>}
+                    {account && <h5>Network id: {chainId}</h5>}
+                </div>
+                <div className="balance-box">
+                    <form onSubmit={handleSubmit}>
+                        <input type="text"
+                               placeholder='Enter wallet address'
+                               name='address'
+                               autoComplete='off'
+                               className='input'
+                               required={true}
+                        />
+                        <button type='submit' className='button'>Get balance</button>
+                    </form>
+                    {!balanceError ? <h3>Result: {value}</h3> : <h3>{balanceError}</h3>}
+                    <p>{errorMessage}</p>
+                </div>
+            </div>
         </div>
     );
 };
